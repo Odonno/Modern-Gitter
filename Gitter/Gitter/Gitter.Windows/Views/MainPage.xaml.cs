c05b6a6c7f061107ee8ede7997b59da10e57a652
Odonno <bottiau.david@laposte.net>
2015-05-02 12:05:47 +0200

    Create Universal app

---
+++ Gitter/Gitter/Gitter.Windows/Views/MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
+using GitHub.Common;
+
+namespace Gitter
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _navigationHelper = new NavigationHelper(this);
+            _navigationHelper.LoadState += navigationHelper_LoadState;
+            _navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+
+        #region Navigation Helper
+
+        private readonly NavigationHelper _navigationHelper;
+
+        /// <summary>
+        /// NavigationHelper est utilisé sur chaque page pour faciliter la navigation et 
+        /// gestion de la durée de vie des processus
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return _navigationHelper; }
+        }
+
+        /// <summary>
+        /// Remplit la page à l'aide du contenu passé lors de la navigation. Tout état enregistré est également
+        /// fourni lorsqu'une page est recréée à partir d'une session antérieure.
+        /// </summary>
+        /// <param name="sender">
+        /// La source de l'événement ; en général <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Données d'événement qui fournissent le paramètre de navigation transmis à
+        /// <see cref="Frame.Navigate(Type, Object)"/> lors de la requête initiale de cette page et
+        /// un dictionnaire d'état conservé par cette page durant une session
+        /// antérieure. L'état n'aura pas la valeur Null lors de la première visite de la page.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Conserve l'état associé à cette page en cas de suspension de l'application ou de la
+        /// suppression de la page du cache de navigation.  Les valeurs doivent être conformes aux
+        /// exigences en matière de sérialisation de <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">La source de l'événement ; en général <see cref="NavigationHelper"/></param>
+        /// <param name="e">Données d'événement qui fournissent un dictionnaire vide à remplir à l'aide de
+        /// état sérialisable.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region Inscription de NavigationHelper
+
+        /// Les méthodes fournies dans cette section sont utilisées simplement pour permettre
+        /// NavigationHelper pour répondre aux méthodes de navigation de la page.
+        /// 
+        /// La logique spécifique à la page doit être placée dans les gestionnaires d'événements pour  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// et <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// Le paramètre de navigation est disponible dans la méthode LoadState 
+        /// en plus de l'état de page conservé durant une session antérieure.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            _navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
