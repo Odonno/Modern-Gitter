0534c993a263dbc013272a8672a6efed027b6d92
Odonno <bottiau.david@laposte.net>
2015-05-15 20:29:13 +0200

    Separate API in 2 projects (Concrete/Fake)

---
+++ Gitter/Gitter.UnitTests/Services/GitterApiTests.cs
@@ -0,0 +1,19 @@
+﻿using Gitter.API.Services.Abstract;
+using Gitter.API.Services.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gitter.UnitTests.Services
+{
+    [TestClass]
+    public class GitterApiTests
+    {
+        private IGitterApiService _gitterApiService;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _gitterApiService = new GitterApiService();
+        }
+    }
+}
