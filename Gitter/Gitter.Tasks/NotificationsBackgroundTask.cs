0993439848182184415718b9ee638739a2ee8fee
Odonno <bottiau.david@laposte.net>
2015-05-31 12:38:17 +0200

    Send toast notifications every time there is unread messages

--- Gitter/Gitter.Tasks/NotificationsBackgroundTask.cs
+++ Gitter/Gitter.Tasks/NotificationsBackgroundTask.cs
@@ -1,5 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
+using Gitter.API.Services.Abstract;
+using Gitter.API.Services.Concrete;
+using Gitter.Services.Abstract;
+using Gitter.Services.Concrete;
 
 namespace Gitter.Tasks
 {
@@ -8,6 +13,25 @@ namespace Gitter.Tasks
         #region Fields
 
         private BackgroundTaskDeferral _deferral;
+        
+        #endregion
+
+
+        #region Services
+
+        private readonly ILocalNotificationService _localNotificationService;
+        private readonly IGitterApiService _gitterApiService;
+
+        #endregion
+
+
+        #region Constructor
+
+        public NotificationsBackgroundTask()
+        {
+            _localNotificationService = new LocalNotificationService();
+            _gitterApiService = new GitterApiService(new ApplicationStorageService());
+        }
 
         #endregion
 
@@ -24,9 +48,20 @@ namespace Gitter.Tasks
         {
             try
             {
-                // TODO : Detect unread chat messages
+                // you need to be authenticated first to get current notifications
+                _gitterApiService.TryAuthenticate();
+
+                // Detect unread chat messages (rooms with unread messages)
+                var roomsWithUnreadMessages = (await _gitterApiService.GetRoomsAsync())
+                    .Where(room => !room.DisabledNotifications && room.UnreadItems > 0);
 
                 // TODO : Update notifications for unread messages (add / update / remove)
+                foreach (var room in roomsWithUnreadMessages)
+                {
+                    // show notifications (toast notifications)
+                    string notificationContent = string.Format("You have {0} unread messages", room.UnreadItems);
+                    _localNotificationService.SendNotification(room.Name, notificationContent);
+                }
             }
             finally
             {
