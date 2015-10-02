257f569c1e3040945ff475f08c21424c3696c63b
Odonno <bottiau.david@laposte.net>
2015-10-02 18:49:27 +0200

    refactor(app): simplify App.xaml.cs file

--- Gitter/Gitter/Gitter.Shared/App.xaml.cs
+++ Gitter/Gitter/Gitter.Shared/App.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
-
-// Pour plus d'informations sur le modèle Application vide, consultez la page http://go.microsoft.com/fwlink/?LinkId=234227
 using GitHub.Common;
 using Newtonsoft.Json;
 using Gitter.Views;
@@ -25,25 +13,23 @@ using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Practices.ServiceLocation;
 using Gitter.ViewModel;
+using Windows.Globalization;
 #if WINDOWS_PHONE_APP
 using GitHub.Services;
 #endif
 
 namespace Gitter
 {
-    /// <summary>
-    /// Fournit un comportement spécifique à l'application afin de compléter la classe Application par défaut.
-    /// </summary>
     public sealed partial class App : Application
     {
         #region Fields
 
 #if WINDOWS_PHONE_APP
-        private static TransitionCollection _transitions;
         private ContinuationManager _continuationManager;
 #endif
 
         #endregion
+
 
         #region Properties
 
@@ -59,11 +45,11 @@ namespace Gitter
 
         #endregion
 
+
         #region Constructor
 
         /// <summary>
-        /// Initialise l'objet d'application de singleton.  Il s'agit de la première ligne du code créé
-        /// à être exécutée. Elle correspond donc à l'équivalent logique de main() ou WinMain().
+        /// Initialize Application (Entry Point)
         /// </summary>
         public App()
         {
@@ -73,12 +59,11 @@ namespace Gitter
 
         #endregion
 
-        #region Launched events
+
+        #region App Lifecycle Events
 
         /// <summary>
-        /// Invoked when the application is launched normally by the end user.
-        /// Other entry points will be used when the application is launched to open a specific file, 
-        /// to display search results, and so forth.
+        /// Invoked when the application is launched normally by the end user
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
         protected override async void OnLaunched(LaunchActivatedEventArgs e)
@@ -93,61 +78,19 @@ namespace Gitter
             var rootFrame = CreateRootFrame();
             await RestoreStatusAsync(e.PreviousExecutionState);
 
-            rootFrame.Navigate(typeof(SplashScreenPage), e.Arguments);
+            rootFrame.Navigate(typeof(SplashScreenPage));
 
             // Ensure the current window is active
             Window.Current.Activate();
 
             // Execute Toast Notification when the app is already launched
             if (ServiceLocator.Current != null)
-            {
                 SelectRoom();
-            }
-        }
-
-        private Frame CreateRootFrame()
-        {
-            var rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                // Set the default language
-                rootFrame = new Frame { Language = Windows.Globalization.ApplicationLanguages.Languages[0] };
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-
-                rootFrame.ContentTransitions = new TransitionCollection();
-            }
-
-            return rootFrame;
-        }
-
-        private async Task RestoreStatusAsync(ApplicationExecutionState previousExecutionState)
-        {
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (previousExecutionState == ApplicationExecutionState.Terminated)
-            {
-                // Restore the saved session state only when appropriate
-                try
-                {
-                    await SuspensionManager.RestoreAsync();
-                }
-                catch (SuspensionManagerException)
-                {
-                    // Something went wrong restoring state.
-                    // Assume there is no state and continue
-                }
-            }
         }
 
 #if WINDOWS_PHONE_APP
         /// <summary>
-        /// Handle OnActivated event to deal with File Open/Save continuation activation kinds
+        /// Handle OnActivated event to handle continuation activation (Authentication)
         /// </summary>
         /// <param name="e">Application activated event arguments, it can be casted to proper sub-type based on ActivationKind</param>
         protected async override void OnActivated(IActivatedEventArgs e)
@@ -173,20 +116,18 @@ namespace Gitter
             }
 
             Window.Current.Activate();
+
+            // Execute Toast Notification when the app is already launched
+            if (ServiceLocator.Current != null)
+                SelectRoom();
         }
 #endif
 
-        #endregion
-
-        #region Suspending Events
-
         /// <summary>
-        /// Appelé lorsque l'exécution de l'application est suspendue.  L'état de l'application est enregistré
-        /// sans savoir si l'application pourra se fermer ou reprendre sans endommager
-        /// le contenu de la mémoire.
+        /// Called when the app is suspended
         /// </summary>
-        /// <param name="sender">Source de la requête de suspension.</param>
-        /// <param name="e">Détails de la requête de suspension.</param>
+        /// <param name="sender">Source of the suspension request</param>
+        /// <param name="e">Details of the suspension request</param>
         private async void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
@@ -196,17 +137,78 @@ namespace Gitter
 
         #endregion
 
+
+        #region Methods
+
+        /// <summary>
+        /// Create a Root Frame for the entire app navigation
+        /// </summary>
+        /// <returns></returns>
+        private Frame CreateRootFrame()
+        {
+            var rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame { Language = ApplicationLanguages.Languages[0] };
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+
+                rootFrame.ContentTransitions = new TransitionCollection();
+            }
+
+            return rootFrame;
+        }
+
+        /// <summary>
+        /// Restore App Status if it was terminated
+        /// </summary>
+        /// <param name="previousExecutionState"></param>
+        /// <returns></returns>
+        private async Task RestoreStatusAsync(ApplicationExecutionState previousExecutionState)
+        {
+            // Do not repeat app initialization when the Window already has content
+            if (previousExecutionState == ApplicationExecutionState.Terminated)
+            {
+                try
+                {
+                    // Restore the saved session state only when appropriate
+                    await SuspensionManager.RestoreAsync();
+                }
+                catch (SuspensionManagerException)
+                {
+                    // Something went wrong restoring state
+                    // Assume there is no state and continue
+                }
+            }
+        }
+
+        #endregion
+
+
         #region Transitions management
 
 #if WINDOWS_PHONE_APP
+        /// <summary>
+        /// Once the app is initialized (after the Splashscreen), reset Transitions
+        /// </summary>
         public static void FirstNavigate()
         {
             var rootFrame = Window.Current.Content as Frame;
             if (rootFrame != null)
-                rootFrame.ContentTransitions = _transitions ?? new TransitionCollection { new NavigationThemeTransition() };
+                rootFrame.ContentTransitions = new TransitionCollection
+                {
+                    new NavigationThemeTransition()
+                };
         }
 #endif
 
+        /// <summary>
+        /// Navigate instantly to the selected room
+        /// </summary>
         public static void SelectRoom()
         {
             // Select room if there is a value in the app launcher
@@ -219,8 +221,12 @@ namespace Gitter
 
         #endregion
 
+
         #region Telemetry management
 
+        /// <summary>
+        /// Start Telemetry client
+        /// </summary>
         public static void StartTelemetry()
         {
 #if DEBUG
