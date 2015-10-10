c366bae384184d3e31b4002e7c772f420cc74281
Odonno <bottiau.david@laposte.net>
2015-10-10 13:54:13 +0200

    refactor(notifications): add a constant about remove notification delay

--- Gitter/Gitter.Configuration/NotificationConstants.cs
+++ Gitter/Gitter.Configuration/NotificationConstants.cs
@@ -6,5 +6,11 @@
         /// Key used to identify notifications coming from the Modern Gitter Application
         /// </summary>
         public static string Tag = "Modern Gitter";
+
+        /// <summary>
+        /// Delay, in seconds, used to clear notification inside the app
+        /// Used in method INotificationService.ClearNotificationGroupAsync()
+        /// </summary>
+        public static double RemoveNotificationDelay = 3;
     }
 }
