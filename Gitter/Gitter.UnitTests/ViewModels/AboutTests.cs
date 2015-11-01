29fb57f6f3054a272a34bbfacfba40574d033302
Odonno <bottiau.david@laposte.net>
2015-11-01 22:01:08 +0100

    chore(app): references packages in Unit Tests project and remove unused packages

---
+++ Gitter/Gitter.UnitTests/ViewModels/AboutTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace Gitter.UnitTests.ViewModels
+{
+    public class AboutTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
