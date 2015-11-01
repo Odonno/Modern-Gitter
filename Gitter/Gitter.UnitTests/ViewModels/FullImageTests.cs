e623b7d3d98b8d9a771b4af9ec750a4b5d3f1310
Odonno <bottiau.david@laposte.net>
2015-11-01 22:22:29 +0100

    tests(fullImage): add tests about Source property

---
+++ Gitter/Gitter.UnitTests/ViewModels/FullImageTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gitter.ViewModel.Concrete;
+using Xunit;
+
+namespace Gitter.UnitTests.ViewModels
+{
+    public class FullImageTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("nothing")]
+        [InlineData("http://www.github.com/octo.png")]
+        public void SetImageSource_Should_ReturnTheSource(string source)
+        {
+            // Arrange
+            var fullImageViewModel = new FullImageViewModel();
+
+            // Act
+            fullImageViewModel.Source = source;
+
+            // Assert
+            Assert.Equal(source, fullImageViewModel.Source);
+        }
+    }
+}
