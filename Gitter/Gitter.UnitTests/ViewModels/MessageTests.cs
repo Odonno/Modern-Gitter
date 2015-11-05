445482fdf685cdeb6cf34d3ed6be95f042c00daf
Odonno <bottiau.david@laposte.net>
2015-11-05 19:53:55 +0100

    tests(message): create test message viewmodel should set message

---
+++ Gitter/Gitter.UnitTests/ViewModels/MessageTests.cs
@@ -0,0 +1,22 @@
+﻿using Gitter.ViewModel.Concrete;
+using GitterSharp.Model;
+using Xunit;
+
+namespace Gitter.UnitTests.ViewModels
+{
+    public class MessageTests
+    {
+        [Fact]
+        public void CreateMessage_Should_SetMessage()
+        {
+            // Arrange
+            var message = new Message();
+            var messageViewModel = new MessageViewModel(message);
+
+            // Act
+
+            // Assert
+            Assert.Same(message, messageViewModel.Message);
+        }
+    }
+}
