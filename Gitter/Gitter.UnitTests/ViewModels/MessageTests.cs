9cfc04919cb19a341626a2251a8d4c5018f5ff9d
Odonno <bottiau.david@laposte.net>
2015-11-05 20:01:01 +0100

    tests(message): create test message viewmodel should set default properties

--- Gitter/Gitter.UnitTests/ViewModels/MessageTests.cs
+++ Gitter/Gitter.UnitTests/ViewModels/MessageTests.cs
@@ -1,5 +1,6 @@
 ﻿using Gitter.ViewModel.Concrete;
 using GitterSharp.Model;
+using System;
 using Xunit;
 
 namespace Gitter.UnitTests.ViewModels
@@ -17,6 +18,31 @@ namespace Gitter.UnitTests.ViewModels
 
             // Assert
             Assert.Same(message, messageViewModel.Message);
+        }
+
+        [Fact]
+        public void CreateMessage_Should_SetDefaultProperties()
+        {
+            // Arrange
+            var message = new Message
+            {
+                Id = "123456",
+                Text = "my message",
+                Html = "<div>my message</div>",
+                SentDate = DateTime.Now,
+                UnreadByCurrent = false
+            };
+            var messageViewModel = new MessageViewModel(message);
+
+            // Act
+
+            // Assert
+            Assert.Equal("123456", messageViewModel.Id);
+            Assert.Equal("my message", messageViewModel.Text);
+            Assert.Equal("<div>my message</div>", messageViewModel.Html);
+            Assert.Equal(DateTime.Now.ToString(), messageViewModel.SentDate.ToString());
+            Assert.Null(messageViewModel.User);
+            Assert.True(messageViewModel.Read);
         }
     }
 }
