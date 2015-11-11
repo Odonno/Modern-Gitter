af09d1714baad8c02fbdc50aa6de72abfd2f5ddb
Odonno <bottiau.david@laposte.net>
2015-11-11 23:56:56 +0100

    chore(tests): create project Fakes to share fakes between Windows and Windows Phone tests projects

---
+++ Gitter/Gitter.WindowsPhone.UnitTests/Fakes/FakeSessionServiceWithException.cs
@@ -0,0 +1,25 @@
+﻿using Gitter.Services.Abstract;
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace Gitter.UnitTests.Fakes
+{
+    public class FakeSessionServiceWithException : ISessionService
+    {
+        public Task<bool?> LoginAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> FinalizeAsync(WebAuthenticationBrokerContinuationEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
