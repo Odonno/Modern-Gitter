082d76d7eadef9bbc47e718c19599dc9449876f4
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2015-11-24 14:10:24 +0100

    feat(app): add german translation
    added a entry to all culturues for the plural char. Before it was "s"
    for english and french, but in german you need a "n".

--- Gitter/Gitter/Gitter.Shared/Converters/DateTimeOffsetToTimeAgoConverter.cs
+++ Gitter/Gitter/Gitter.Shared/Converters/DateTimeOffsetToTimeAgoConverter.cs
@@ -20,21 +20,21 @@ namespace Gitter.Converters
             if (timeSpanDiff.TotalSeconds < 60)
                 return string.Format(_resourceLoader.GetString("SecondsAgo"),
                     timeSpanDiff.Seconds,
-                    timeSpanDiff.Seconds > 1 ? "s" : "");
+                    timeSpanDiff.Seconds > 1 ? _resourceLoader.GetString("PluralChar") : "");
 
             if (timeSpanDiff.TotalMinutes < 60)
                 return string.Format(_resourceLoader.GetString("MinutesAgo"),
                     timeSpanDiff.Minutes,
-                    timeSpanDiff.Minutes > 1 ? "s" : "");
+                    timeSpanDiff.Minutes > 1 ? _resourceLoader.GetString("PluralChar") : "");
 
             if (timeSpanDiff.TotalHours < 24)
                 return string.Format(_resourceLoader.GetString("HoursAgo"),
                     timeSpanDiff.Hours,
-                    timeSpanDiff.Hours > 1 ? "s" : "");
+                    timeSpanDiff.Hours > 1 ? _resourceLoader.GetString("PluralChar") : "");
 
             return string.Format(_resourceLoader.GetString("DaysAgo"),
                 timeSpanDiff.Days,
-                timeSpanDiff.Days > 1 ? "s" : "");
+                timeSpanDiff.Days > 1 ? _resourceLoader.GetString("PluralChar") : "");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
